d77132f39369596a160e631a742a512be1e0afb0
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2020-12-17 09:35:28 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+var num1 = 2
+var num2 = 31
 
-// Write your code here
+
+const multiply = (num1 * num2)
+
+const random = Math.floor(Math.random()* 100) + 1
+
+const mod = 16 % 6
+
+const max = Math.max( 5, 10, 15, 20)
